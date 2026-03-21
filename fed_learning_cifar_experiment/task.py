6571cd731bfcced02e723a004cbcc38471076f76
srinisvas <subramaniansrinivasan92@gmail.com>
2026-03-20 21:53:46 -0400

    adjust Dirichlet alpha value to 0.5

--- fed_learning_cifar_experiment/task.py
+++ fed_learning_cifar_experiment/task.py
@@ -327,7 +327,7 @@ def load_data(partition_id: int, num_partitions: int, alpha_val: float, backdoor
             dirichlet_cache = dirichlet_indices(
                 labels=labels,
                 num_partitions=num_partitions,
-                alpha=0.1,
+                alpha=0.5,
                 seed=42,
             )
 
